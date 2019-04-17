703f63e56c551533ce3786dcb4f123e98af1a8e9
Thierry Joux <tjoux@hotmail.com>
2019-04-17 14:13:06 -0700

    cycling through images no fading or sliding

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,63 @@
-class Img {
-    constructor(img) {
-        this.img = img;
+class ImageItem {
+    constructor(imageName) {
+        this.imageDiv = document.createElement('div')
+        this.imageDiv.classList.add("image");
+        this.img = document.createElement('img');
+        this.img.src = imageDirectory + imageName;
+        this.img.alt = altText;
+        
+        this.imageDiv.appendChild(this.img);
+    }
+
+    showImage() {
+        document.querySelector('.bottom-content').appendChild(this.imageDiv);
+    }
+
+    removeImage() {
+        document.querySelector('.bottom-content').removeChild(this.imageDiv);
     }
 }
 
+ //display next 4 images
+function showFourImages(imageArray) {
+
+    for(let i=imageIndex; i < imageIndex+4; i++) {
+        imageArray[i % imageArray.length].showImage();
+    }
+}
+
+//remove current 4 images
+function removeFourImages(imageArray) {
+    for (let i=imageIndex; i < imageIndex+4; i++) {
+        imageArray[i % imageArray.length].removeImage();
+    }
+    //update starting index of 4 new images to display
+    imageIndex = (imageIndex + 4) % imageArray.length;
+}
+
+function cycleImages(imageArray) {
+
+    showFourImages(imageArray);
+    setInterval(function() {
+        removeFourImages(imageArray);
+        showFourImages(imageArray);
+    }, 7000);
+}
+
+
 //Hardcoding images into objects
-imgArray = []
-let img1 = document.createElement('img');
+const imageDirectory = "./img/carousel/";
+const altText = "Images of people or places abroad"
+
+let imageIndex = 0
+
+imageArray = []
+
+imageArray.push(new ImageItem("deanna-ritchie-227649-unsplash.jpg"));
+imageArray.push(new ImageItem("anders-jilden-307322-unsplash.jpg"));
+imageArray.push(new ImageItem("juan-rojas-349568-unsplash.jpg"));
+imageArray.push(new ImageItem("vaida-tamosauskaite-85608-unsplash.jpg"));
+imageArray.push(new ImageItem("mantas-hesthaven-135478-unsplash.jpg"));
+
+//setInterval( cycleImages(imageArray), 3000 );
+cycleImages(imageArray);
